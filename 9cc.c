65180881865f45e69c91a63313436648e8c34168
foooomio <foooomio@gmail.com>
2019-05-23 12:47:14 +0900

    単項プラスと単項マイナスを実装

--- 9cc.c
+++ 9cc.c
@@ -87,6 +87,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *term();
 
 Node *expr() {
@@ -103,16 +104,24 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = term();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node('*', node, term());
+      node = new_node('*', node, unary());
     else if (consume('/'))
-      node = new_node('/', node, term());
+      node = new_node('/', node, unary());
     else
       return node;
   }
+}
+
+Node *unary() {
+  if (consume('+'))
+    return term();
+  if (consume('-'))
+    return new_node('-', new_node_num(0), term());
+  return term();
 }
 
 Node *term() {
